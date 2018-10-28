ffbf5cab6463710e72c983647ed9f4929eac8a3f
Stephen Feest <steve84@gmail.com>
2018-10-28 18:13:40 +0000

    Add solution to exercise 2

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -3,7 +3,24 @@ import UserService from '../services/user_service.js';
 function getFollowers(emailInput, outputTextArea, submitButton) {
   const userService = new UserService();
 
-  // TODO
+  submitButton.disabled = true;
+
+  userService
+    .findByEmail(emailInput.value)
+    .then(({ followers }) => Promise.all(
+      followers.map(followerEmail =>
+        getLocation(followerEmail).then(location => `${followerEmail} (${location})`))))
+    .then(followersWithLocations => {
+      outputTextArea.value = followersWithLocations.join('\n');
+      submitButton.disabled = false;
+    });
+
+  function getLocation(email) {
+    return userService
+      .findByEmail(email)
+      .then(user => user.location)
+      .catch(() => 'location unavailable');
+  }
 }
 
 document.getElementById('ex2').addEventListener('submit', event => {
